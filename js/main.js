0c7857fd6a0176c5670540356c3c4daf9c1c4e97
Marco Cedaro <cedmax@users.noreply.github.com>
2013-03-08 10:15:38 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+require([
+        "jquery",
+        "lib/smoke.min",
+        "lib/backstretch.min",
+        "lib/jrumble.min",
+        "lib/atooltip.min"
+    ], function($, smoke) {
+        'use strict';
+
+        var imgArray = [
+            {name: 'me', align: 'left', centeredX: false},
+            {name: 'me2', align: 'right', centeredX: false},
+            {name: 'me3', align: 'right', centeredX: true}
+        ];
+
+        var img =  imgArray[Math.floor(Math.random()*imgArray.length)];
+        $.backstretch("img/"+ img.name +".jpg", {align: img.align, centeredX:img.centeredX, centeredY:false, speed: 300});
+
+        $("#content a").aToolTip({xOffset: 15, yOffset: -65 }).jrumble();
+
+        $("#content a[rel=smoke]").click(function(){
+            smoke.signal('see you soon...');
+        });
+});
