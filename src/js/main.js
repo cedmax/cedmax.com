ed55c51772d6ad6df39b5f96e59a9880df539085
cedmax <marco.cedaro@gmail.com>
2014-11-11 21:35:49 +0000

    changed sprite generator, added almondjs, removed some links, hidden g+

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,42 @@
-require([
-        "jquery",
-        "lib/smoke.min",
-        "lib/backstretch.min",
-        "lib/jrumble.min",
-        "lib/atooltip.min"
-    ], function($, smoke) {
-        'use strict';
+/* global require */
 
-        var imgArray = [
-            {name: 'me', align: 'left', centeredX: false},
-            {name: 'me2', align: 'right', centeredX: false},
-            {name: 'me3', align: 'right', centeredX: true}
-        ];
+require( [
+	'jquery',
+	'lib/smoke.min',
+	'lib/backstretch.min',
+	'lib/jrumble.min',
+	'lib/atooltip.min'
+], function( $, smoke ) {
+	'use strict';
 
-        var img =  imgArray[Math.floor(Math.random()*imgArray.length)];
-        $.backstretch("img/"+ img.name +".jpg", {align: img.align, centeredX:img.centeredX, centeredY:false, speed: 300});
+	var imgArray = [ {
+		name: 'me',
+		align: 'left',
+		centeredX: false
+	}, {
+		name: 'me2',
+		align: 'right',
+		centeredX: false
+	}, {
+		name: 'me3',
+		align: 'right',
+		centeredX: true
+	} ];
 
-        $("#content a").aToolTip({xOffset: 15, yOffset: -65 }).jrumble();
+	var img = imgArray[ Math.floor( Math.random() * imgArray.length ) ];
+	$.backstretch( 'img/' + img.name + '.jpg', {
+		align: img.align,
+		centeredX: img.centeredX,
+		centeredY: false,
+		speed: 300
+	} );
 
-        $("#content a[rel=smoke]").click(function(){
-            smoke.signal('see you soon...');
-        });
-});
+	$( '#content a:not([class~="google" ])' ).aToolTip( {
+		xOffset: 15,
+		yOffset: -65
+	} ).jrumble();
+
+	$( '# content a[ rel = smoke ]' ).click( function() {
+		smoke.signal( 'see you soon...' );
+	} );
+} );
