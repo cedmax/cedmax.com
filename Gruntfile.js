9928d271ad3fef1942f079afd47aa9e3778c874d
cedmax <marco.cedaro@gmail.com>
2015-02-13 14:28:57 +0000

    bump version

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function( grunt ) {
 	grunt.loadNpmTasks( 'grunt-htmlrefs' );
 	grunt.loadNpmTasks( 'grunt-spritesmith' );
 
-	var version = 3;
+	var version = 4;
 
 	grunt.initConfig( {
 		htmlrefs: {
