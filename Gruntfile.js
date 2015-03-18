05a27301fd3445beb52817502172c75ae8ded312
cedmax <marco.cedaro@gmail.com>
2015-03-18 23:12:39 +0000

    better assets versioning

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,10 @@
 module.exports = function( grunt ) {
 	'use strict';
-	
+
 	require('matchdep').filterDev('grunt-!(cli)').forEach(grunt.loadNpmTasks);
 
-	var version = 5;
+	var assetsVersion = 5;
+	var imagesVersion = 1;
 
 	grunt.initConfig( {
 		htmlrefs: {
@@ -11,7 +12,8 @@ module.exports = function( grunt ) {
 				src: 'src/index.html',
 				dest: 'index.html',
 				options: {
-					version: version,
+					assetsVersion: assetsVersion,
+					imagesVersion: imagesVersion,
 					includes: {
 						bundle: './src/partials/scripts.html'
 					}
@@ -23,7 +25,7 @@ module.exports = function( grunt ) {
 				files: [ {
 					expand: true,
 					flatten: true,
-					src: [ 'tmp/main.' + version + '.js' ],
+					src: [ 'tmp/main.' + assetsVersion + '.js' ],
 					dest: 'js/',
 					filter: 'isFile'
 				}, {
@@ -35,7 +37,7 @@ module.exports = function( grunt ) {
 				}, {
 					expand: true,
 					flatten: true,
-					src: [ 'tmp/img/sprite.' + version + '.png' ],
+					src: [ 'tmp/img/sprite.' + imagesVersion + '.png' ],
 					dest: 'img',
 					filter: 'isFile'
 				}, {
@@ -45,23 +47,26 @@ module.exports = function( grunt ) {
 					dest: 'img',
 					filter: 'isFile',
 					rename: function(dest, src) {
-						return dest +'/'+ src.replace('.jpg', '.'+ version+'.jpg');
+						return dest +'/'+ src.replace('.jpg', '.'+ imagesVersion+'.jpg');
 					}
 				}, {
 					expand: true,
 					flatten: true,
 					src: [ 'src/img/gif/*.*' ],
 					dest: 'img/gif',
-					filter: 'isFile'
+					filter: 'isFile',
+					rename: function(dest, src) {
+						return dest +'/'+ src.replace('.gif', '.'+ imagesVersion+'.gif');
+					}
 				}  ]
 			}
 		},
 		sprite: {
 			all: {
 				src: 'src/img/used-icons/*.png',
-				destImg: 'img/sprite.' + version + '.png',
+				destImg: 'img/sprite.' + imagesVersion + '.png',
 				destCSS: 'tmp/icons.css',
-				imgPath: 'img/sprite.' + version + '.png',
+				imgPath: 'img/sprite.' + imagesVersion + '.png',
 				'cssOpts': {
 					'cssClass': function( item ) {
 						return '.scl.' + item.name;
@@ -74,7 +79,7 @@ module.exports = function( grunt ) {
 				options: {
 					optimize: 'uglify2',
 					baseUrl: '.',
-					out: 'tmp/main.' + version + '.js',
+					out: 'tmp/main.' + assetsVersion + '.js',
 					name: 'src/js/main.js',
 					paths: {
 						'lib': 'src/js/lib',
