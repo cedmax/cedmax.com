295d6e662b32c777a41c62a4006aa00e69dda6e8
Marco Cedaro <cedmax@Marcos-MacBook-Pro.local>
2016-02-05 20:59:08 +0000

    added 2016

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function( grunt ) {
 	var swPrecache = require('sw-precache');
 	var path = require('path');
 	
-	var cacheVersion = 4;
+	var cacheVersion = 10;
 
 	grunt.registerMultiTask('swPrecache', function(){
 		var done = this.async();
