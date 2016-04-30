146571d467a12ba05913a1bf2489ca31257519db
Marco Cedaro <cedmax@users.noreply.github.com>
2016-04-30 15:50:13 +0100

    bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function( grunt ) {
 	var swPrecache = require('sw-precache');
 	var path = require('path');
 
-	var cacheVersion = 10;
+	var cacheVersion = 11;
 
 	grunt.registerMultiTask('swPrecache', function(){
 		var done = this.async();
