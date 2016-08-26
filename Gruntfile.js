44f1a4372f93dc040b8d795a20b9eda03fc0ea58
cedmax <cedmax@users.noreply.github.com>
2016-08-26 20:04:52 +0100

    added credits

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function( grunt ) {
 	var swPrecache = require('sw-precache');
 	var path = require('path');
 
-	var cacheVersion = 17;
+	var cacheVersion = 18;
 
 	grunt.registerMultiTask('swPrecache', function(){
 		var done = this.async();
