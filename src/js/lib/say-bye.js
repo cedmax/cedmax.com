40148ffb06722e09699ea6a2df427a4742db2a4a
Marco Cedaro <cedmax@cedmachine.local>
2016-12-10 17:39:35 +0000

    fix the goodbye, after introducing svg

--- src/js/lib/say-bye.js
+++ src/js/lib/say-bye.js
@@ -6,12 +6,12 @@ define([
 
   return function(){
     document.body.addEventListener('click', function(event) {
-      if (event.target.getAttribute('rel') === 'smoke') {
+      if (event.target.getAttribute('rel') === 'smoke' ||
+        event.target.parentNode.getAttribute('rel') === 'smoke') {
         smoke.signal('see you soon...');
         document.querySelector('#innerced image').setAttribute('y', document.querySelector('#innerced image').getAttribute('y') - 150);
       }
     });
-
     helpers.ready(smoke.bodyload);
   };
 });
