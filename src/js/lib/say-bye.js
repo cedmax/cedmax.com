b64b58ca613f334428c3b8709859be2fa9814cef
cedmax <cedmax@users.noreply.github.com>
2016-12-10 15:14:42 +0000

    refactoring

---
+++ src/js/lib/say-bye.js
@@ -0,0 +1,17 @@
+define([
+  'vendor/smoke.min',
+  'lib/helpers'
+], function(smoke, helpers) {
+  'use strict';
+
+  return function(){
+    document.body.addEventListener('click', function(event) {
+      if (event.target.getAttribute('rel') === 'smoke') {
+        smoke.signal('see you soon...');
+        document.querySelector('#innerced image').setAttribute('y', document.querySelector('#innerced image').getAttribute('y') - 150);
+      }
+    });
+
+    helpers.ready(smoke.bodyload);
+  };
+});
