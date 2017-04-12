9a36715a2daa2cd68a51b5722bed624142d40853
Marco Cedaro <marco.cedaro@gmail.com>
2017-04-12 20:54:08 +0300

    changed caching strategy to be fastest over network first

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,6 @@
 importScripts('/node_modules/sw-toolbox/sw-toolbox.js');
 
 toolbox.precache(@@filesToCache)
-toolbox.router.get('/', toolbox.networkFirst);
-toolbox.router.get('js/main.*.js', toolbox.networkFirst);
+toolbox.router.get('/', toolbox.fastest);
+toolbox.router.get('js/main.*.js', toolbox.fastest);
 toolbox.router.get('img/*', toolbox.cacheFirst);
