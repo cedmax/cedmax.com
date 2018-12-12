a079952dc63f5d050c81a8888fcbbd7476a5c480
Marco Cedaro <cedmax@users.noreply.github.com>
2018-12-12 23:21:12 +0000

    building in a dist folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,27 @@
-module.exports = function( grunt ) {
+module.exports = function(grunt) {
   'use strict';
 
-  require('matchdep').filterDev('grunt-!(cli)').forEach(grunt.loadNpmTasks);
+  require('matchdep')
+    .filterDev('grunt-!(cli)')
+    .forEach(grunt.loadNpmTasks);
   var fs = require('fs');
   var cacheVersion = 33;
 
-  grunt.initConfig( {
+  grunt.initConfig({
     htmlrefs: {
       dist: {
         src: 'src/index.html',
-        dest: 'index.html',
+        dest: 'dist/index.html',
         options: {
           cacheVersion: cacheVersion,
           includes: {
             bundle: './src/partials/scripts.html',
             schema: './src/partials/schema.html',
             meta: './src/partials/meta.html',
-            closeimg: './src/partials/closeimg.html'
-          }
-        }
-      }
+            closeimg: './src/partials/closeimg.html',
+          },
+        },
+      },
     },
     replace: {
       serviceWorker: {
@@ -27,58 +29,80 @@ module.exports = function( grunt ) {
           patterns: [
             {
               match: 'filesToCache',
-              replacement: fs.readdirSync(__dirname + '/src/img').filter(img => img.indexOf('.jpg')>-1 || img.indexOf('.png')>-1).map(img => 'img/' + img.replace('.', `.${cacheVersion}.`))
-            }
-          ]
+              replacement: fs
+                .readdirSync(__dirname + '/src/img')
+                .filter(
+                  img => img.indexOf('.jpg') > -1 || img.indexOf('.png') > -1
+                )
+                .map(img => 'img/' + img.replace('.', `.${cacheVersion}.`)),
+            },
+          ],
         },
         files: [
-          { expand: true, flatten: true, src: [ 'src/service-worker.js' ], dest: './' }
-        ]
-      }
-   },
+          {
+            expand: true,
+            flatten: true,
+            src: ['src/service-worker.js'],
+            dest: './dist',
+          },
+        ],
+      },
+    },
     copy: {
       main: {
-        files: [ {
-          expand: true,
-          flatten: true,
-          src: [ 'tmp/main.' + cacheVersion + '.js' ],
-          dest: 'js/',
-          filter: 'isFile'
-        }, {
-          expand: true,
-          flatten: true,
-          src: [ 'src/media/*' ],
-          dest: 'media',
-          filter: 'isFile'
-        }, {
-          expand: true,
-          flatten: true,
-          src: [ 'src/img/*.*' ],
-          dest: 'img',
-          filter: 'isFile',
-          rename: function(dest, src) {
-            return dest +'/'+ src.replace('.jpg', '.'+ cacheVersion+'.jpg');
-          }
-        }, {
-          expand: true,
-          flatten: true,
-          src: [ 'src/img/gif/*.*' ],
-          dest: 'img/gif',
-          filter: 'isFile',
-          rename: function(dest, src) {
-            return dest +'/'+ src.replace('.gif', '.'+ cacheVersion+'.gif');
-          }
-        }, {
-          expand: true,
-          flatten: true,
-          src: [ 'src/img/pp/*.*' ],
-          dest: 'img/pp',
-          filter: 'isFile',
-          rename: function(dest, src) {
-            return dest +'/'+ src.replace('.jpg', '.'+ cacheVersion+'.jpg');
-          }
-        }  ]
-      }
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: ['tmp/main.' + cacheVersion + '.js'],
+            dest: 'dist/js/',
+            filter: 'isFile',
+          },
+          {
+            expand: true,
+            flatten: true,
+            src: ['src/media/*'],
+            dest: 'dist/media',
+            filter: 'isFile',
+          },
+          {
+            expand: true,
+            flatten: true,
+            src: ['src/img/*.*'],
+            dest: 'dist/img',
+            filter: 'isFile',
+            rename: function(dest, src) {
+              return (
+                dest + '/' + src.replace('.jpg', '.' + cacheVersion + '.jpg')
+              );
+            },
+          },
+          {
+            expand: true,
+            flatten: true,
+            src: ['src/img/gif/*.*'],
+            dest: 'dist/img/gif',
+            filter: 'isFile',
+            rename: function(dest, src) {
+              return (
+                dest + '/' + src.replace('.gif', '.' + cacheVersion + '.gif')
+              );
+            },
+          },
+          {
+            expand: true,
+            flatten: true,
+            src: ['src/img/pp/*.*'],
+            dest: 'dist/img/pp',
+            filter: 'isFile',
+            rename: function(dest, src) {
+              return (
+                dest + '/' + src.replace('.jpg', '.' + cacheVersion + '.jpg')
+              );
+            },
+          },
+        ],
+      },
     },
     requirejs: {
       compile: {
@@ -89,52 +113,62 @@ module.exports = function( grunt ) {
           out: 'tmp/main.' + cacheVersion + '.js',
           name: 'src/js/main.js',
           paths: {
-            'lib': 'src/js/lib',
-            'vendor': 'src/js/vendor',
+            lib: 'src/js/lib',
+            vendor: 'src/js/vendor',
           },
           shim: {
             'vendor/webcredits.min': {
-              exports: 'webCredits'
+              exports: 'webCredits',
             },
             'vendor/smoke.min': {
-              exports: 'smoke'
+              exports: 'smoke',
             },
             'vendor/giflinks.min': {
-              exports: 'GifLinks'
+              exports: 'GifLinks',
             },
-          }
-        }
-      }
+          },
+        },
+      },
     },
     cssmin: {
       combine: {
         files: {
-          'tmp/style.css': [ 'src/style.css' ],
-          'tmp/fonts.css': [ 'src/fonts.css' ]
-        }
-      }
+          'tmp/style.css': ['src/style.css'],
+          'tmp/fonts.css': ['src/fonts.css'],
+        },
+      },
     },
     minifyHtml: {
       options: {
-        quotes: true
+        quotes: true,
       },
       dist: {
         files: {
-          'index.html': 'index.html'
-        }
-      }
+          'dist/index.html': 'dist/index.html',
+        },
+      },
     },
-    autoprefixer:{
+    autoprefixer: {
       default: {
         src: 'tmp/style.css',
-        dest: 'tmp/style.css'
+        dest: 'tmp/style.css',
       },
     },
     clean: {
-      tmp: [ 'tmp' ],
-      reset: ['media', 'img', 'js', 'service-worker*', 'index.html']
-    }
-  } );
+      tmp: ['tmp'],
+      reset: ['media', 'img', 'js', 'service-worker*', 'index.html'],
+    },
+  });
 
-  grunt.registerTask( 'default', [ 'clean:reset', 'requirejs', 'cssmin', 'autoprefixer', 'copy', 'htmlrefs', 'minifyHtml', 'clean:tmp', 'replace' ] );
+  grunt.registerTask('default', [
+    'clean:reset',
+    'requirejs',
+    'cssmin',
+    'autoprefixer',
+    'copy',
+    'htmlrefs',
+    'minifyHtml',
+    'clean:tmp',
+    'replace',
+  ]);
 };
