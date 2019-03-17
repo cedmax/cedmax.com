17f03f96a08dcfef9d3330f0aca6f471eed8afe6
Marco Cedaro <cedmax@users.noreply.github.com>
2019-03-17 21:50:17 +0000

    favicon and viewport

--- src/containers/Meta.js
+++ src/containers/Meta.js
@@ -10,5 +10,11 @@ export default ({ meta }) => (
     <meta property="og:title" content={meta.meta_title} />
     <meta property="og:description" content={meta.meta_decription} />
     <meta name="twitter:site" content="@cedmax" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, maximum-scale=1"
+    />
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
   </Head>
 );
